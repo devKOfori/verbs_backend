2235f6b74e0396b649f8be264f602efe9e031306
devKOfori <groupfivethree@gmail.com>
2024-11-14 11:15:56 +0200

    Modify project settings

--- verbs/settings.py
+++ verbs/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api.apps.ApiConfig",
     "rest_framework",
+    "django_filters",
     "oauth2_provider",
     "social_django",
     "drf_social_oauth2",
@@ -141,7 +142,8 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
         "drf_social_oauth2.authentication.SocialAuthentication",
-    )
+    ),
+    # "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]
 }
 
 AUTHENTICATION_BACKENDS = (
