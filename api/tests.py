32d618d00ca1c3f8a924a474f67905fc6ffa2167
devKOfori <groupfivethree@gmail.com>
2024-11-18 10:07:44 +0200

    Create test for creating product with valid user credentials

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,4 @@
+import requests
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from .models import Colleague, ResetPassword
@@ -113,6 +114,31 @@ class ResetPasswordTests(APITestCase):
         self.assertEqual(response.status_code, 400)
 
 
+class ProductTests(APITestCase):
+
+    def test_create_product_with_auth_user(self):
+        tokens = {
+            "refresh": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTczMTkxOTEzNywiaWF0IjoxNzMxODMyNzM3LCJqdGkiOiIwZjJlNDI0ZjE0N2M0M2U5YmFkMDY3NGIyZGZjOTIyYSIsInVzZXJfaWQiOiJiZThlNjhkOC1jMTQ4LTQ3MGItYjJlZS1kMTQzZTI0NzlkOWEifQ.XdR997XH6E7MeTlbMWXhYDDLJUV2uEe8xkBR0j3KwcM",
+            "access": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzMxODMzMDM3LCJpYXQiOjE3MzE4MzI3MzcsImp0aSI6IjkwZDgzOWFjNmMxYjRmYTY4NTNiN2VkMjk3OTJhYTk5IiwidXNlcl9pZCI6ImJlOGU2OGQ4LWMxNDgtNDcwYi1iMmVlLWQxNDNlMjQ3OWQ5YSJ9.yOzXgdJJjeoE17bKW1YcnT_6hIgr4VN2yw_DBKTAa-o",
+        }
+        payload = {
+            "name": "",
+            "product_type": {"name": ""},
+            "grade": {"name": ""},
+            "themes": [],
+            "sizes": [],
+            "weight": null,
+            "colors": [],
+            "frame_types": [],
+            "unit_price": null,
+            "qty": null,
+            "description": "",
+            "images": [],
+            "return_policy": "",
+        }
+        requests.post("http://localhost:8000/api/products/add/", json={})
+
+
 class OrderTests(APITestCase):
     def setUp(self) -> None:
         pass
@@ -120,17 +146,13 @@ class OrderTests(APITestCase):
     def test_create_order(self):
         url = reverse("create-order")
         data = {
-            "items": [{"id": "bb6ccdd4-3218-4794-a16a-9327cdfec56f", "qty":1}],
+            "items": [{"id": "bb6ccdd4-3218-4794-a16a-9327cdfec56f", "qty": 1}],
             "order_date": "2024-11-15",
-            "promo_code": {
-                "code": ""
-            },
-            "shipping_info": {
-                "shipping_address": "pursitie 7 F"
-            },
+            "promo_code": {"code": ""},
+            "shipping_info": {"shipping_address": "pursitie 7 F"},
             "first_name": "Ebenezer",
             "last_name": "Ofori-Mensah",
-            "email": "oforimensahebenezer07@gmail.com"
+            "email": "oforimensahebenezer07@gmail.com",
         }
         response = self.client.post(url, data, format="json")
         print(response.data)
