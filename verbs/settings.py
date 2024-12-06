aace1ee7d9ec6d113eb5c08e54991714bfb6d950
devKOfori <groupfivethree@gmail.com>
2024-12-06 20:27:30 +0200

    Update settings file

--- verbs/settings.py
+++ verbs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -24,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ["testserver", "localhost", "verbsmerch.pythonanywhere.com"]
 
